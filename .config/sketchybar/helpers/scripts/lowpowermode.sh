dd0b18d870eeabcf6aecaf2386331aa8d9be61e7
wiggly-sheets <zmikula@icloud.com>
2025-09-26 06:58:21 -0400

    sbar media item

--- .config/sketchybar/helpers/scripts/lowpowermode.sh
+++ .config/sketchybar/helpers/scripts/lowpowermode.sh
@@ -5,7 +5,7 @@ mode=$(pmset -g | grep lowpowermode | grep -o '[01]')
 
 if [ "$mode" = "1" ]; then
   # Low power mode enabled: green lightning bolt
-  sketchybar --set lowpowermode label.color="0xff1c924b"
+  sketchybar --set lowpowermode label.color="0xff63981f"
 else
   # Low power mode disabled: orange lightning bolt
   sketchybar --set lowpowermode label.color="0xffe3a500"
