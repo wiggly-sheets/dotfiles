1d13e52d59955ec5a707e8b3d6b300e16eac9de9
wiggly-sheets <zmikula@icloud.com>
2025-10-18 08:48:17 -0400

    more right click click scripts

--- .config/sketchybar/items/battery.lua
+++ .config/sketchybar/items/battery.lua
@@ -49,7 +49,7 @@ local battery_percentage = sbar.add("item", "items.battery_percentage", {
 
 local battery = sbar.add("item", "items.battery", {
 	position = "right",
-	padding_right = -30,
+	padding_right = -32,
 	padding_left = 2,
 	y_offset = -5,
 	icon = {
