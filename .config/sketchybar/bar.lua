2afbf784bbfaebd944eb940c648b12044b370947
wiggly-sheets <zmikula@icloud.com>
2025-11-28 23:22:30 -0500

    new statusbar for tmux and nvim

--- .config/sketchybar/bar.lua
+++ .config/sketchybar/bar.lua
@@ -1,9 +1,11 @@
 ---- Equivalent to the --bar domain
+local colors = require("colors")
 sbar.bar({
 
 	display = "main",
 	height = 40,
-	color = 0x0A0A0A0D,
+	--	color = 0x0A0A0A0D,
+	color = colors.transparent,
 	blur_radius = 10,
 	corner_radius = 0,
 	padding_right = 0,
