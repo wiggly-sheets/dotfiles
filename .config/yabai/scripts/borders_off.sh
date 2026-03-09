755cb889517ea22fe129fbb0a07c38124a0c2c7b
wiggly-sheets <zmikula@icloud.com>
2026-03-09 11:46:26 -0400

    edited borders toggles for yabai and added standalone borders config

--- .config/yabai/scripts/borders_off.sh
+++ .config/yabai/scripts/borders_off.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 brew services stop borders && killall borders
+yabai -m config top_padding 0
+yabai -m config bottom_padding 0
+yabai -m config right_padding 0
+yabai -m config left_padding 0
--- .config/yabai/scripts/borders_on.sh
+++ .config/yabai/scripts/borders_on.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 brew services start borders
-yabai -m config top_padding 2
+yabai -m config top_padding 1
 yabai -m config bottom_padding 1
 yabai -m config right_padding 1
 yabai -m config left_padding 1
