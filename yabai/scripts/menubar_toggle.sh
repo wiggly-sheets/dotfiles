43e233892c9a59130dc3792a86ee06f8bdd916cf
wiggly-sheets <zmikula@icloud.com>
2026-03-31 09:23:47 -0400

    add space renaming script to yabairc for autorun at login/dock restart

--- yabai/scripts/menubar_toggle.sh
+++ yabai/scripts/menubar_toggle.sh
@@ -7,9 +7,10 @@ o=$(yabai -m config menubar_opacity)
 if awk "BEGIN{exit !($o>=1)}"; then
     # If opacity >= 1, hide menubar and show sketchybar
     yabai -m config menubar_opacity 0.0
-    sketchybar --bar hidden=false
+    sketchybar --bar hidden=false y_offset=-50
+    sketchybar --animate sin 12 --bar y_offset=6
 else
     # If opacity < 1, show menubar and hide sketchybar
-    yabai -m config menubar_opacity 1.0
     sketchybar --bar hidden=true
+    yabai -m config menubar_opacity 1.0
 fi
