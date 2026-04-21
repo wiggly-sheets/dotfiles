67476c37be5b290adeaa90b21f8f3cfb8e6a207d
wiggly-sheets <zmikula@icloud.com>
2026-04-21 09:21:34 -0400

    bbedit opinionated sample setup & theme collection

--- yabai/scripts/opacity.sh
+++ yabai/scripts/opacity.sh
@@ -5,6 +5,8 @@ opacity=$(yabai -m config active_window_opacity)
 
 if [ "$opacity" = "1.0000" ]; then
 yabai -m config active_window_opacity 0.95
+yabai -m config normal_window_opacity .70
 else
 yabai -m config active_window_opacity 1.0
+yabai -m config normal_window_opacity .80
 fi
